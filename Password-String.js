5acbdd42c3684f748e1fbed02ab1e9e44107bcec
SAHIL141299 <158707990+SAHIL141299@users.noreply.github.com>
2024-06-11 16:55:55 +0530

    Password-string

---
+++ Password-String.js
@@ -0,0 +1,13 @@
+let fs = require("fs");
+let filename = "password.txt";
+let Fetchdata = fs.readFileSync(process.cwd() + "/" + filename).toString();
+let array = fs.readFileSync("password.txt", "utf8").split("\n");
+
+array.map((item) => {
+  let userID;
+  const myArray = item.split(":");
+  myArray[3] > 500 ? (userID = ", userID :" + myArray[3]) : (userID = "");
+  console.log(
+    `User: ${myArray[0]},Command interpreter: ${myArray[6]} ${userID}`
+  );
+});
